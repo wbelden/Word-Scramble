a3e6c2fd2ae450350c2bc4307a0f606185ec9b59
Wade Belden <wade.belden1@gmail.com>
2021-03-18 23:17:01 -0500

    Initial Commit

---
+++ Shared/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Shared
+//
+//  Created by Wade Belden on 3/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
